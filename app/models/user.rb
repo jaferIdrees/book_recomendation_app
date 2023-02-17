f984fbd9edfb0842b9c0f2fb612ee8774b059b11
jaferidrees <jaferll@gmail.com>
2023-02-17 21:24:29 +0100

    Add user full name validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :user_libraries
   has_many :books, through: :user_libraries
+
+  validates :full_name, presence: true,
+                         length: { maximum: 32, too_long: '%<count>s characters is the maximum allowed' }
 end

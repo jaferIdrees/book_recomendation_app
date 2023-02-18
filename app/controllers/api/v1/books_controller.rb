36fa063d84c3b8aba069dd0e9670bbae78f41bbc
jaferidrees <jaferll@gmail.com>
2023-02-18 06:54:51 +0100

    Update books_controller handle missing publication date

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -18,12 +18,17 @@ class Api::V1::BooksController < ApplicationController
     if @book.nil?
       author = Author.find_by(name: book_params[:author])
       author = Author.create(name: book_params[:author]) if author.nil?
+      begin
+        publication_date= book_params[:publication_date].to_date
+     rescue ArgumentError
+        publication_date= nil
+     end
       book_data = {
         title: book_params[:title],
         author: author,
         first_sentence: book_params[:first_sentence], lang: book_params[:lang], publisher: book_params[:publisher],
         cover_image: book_params[:cover_image], isbn: book_params[:isbn],
-        publication_date: book_params[:publication_date].to_date,
+        publication_date: publication_date,
       }
       @book = Book.new(book_data)
       @book.save
